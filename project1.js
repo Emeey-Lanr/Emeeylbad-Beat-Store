9dd02b73ef38f9e0496340bd3cf31bedcadca47b
Oyelowo Emmanuel <emeeylanr04@gmail.com>
2022-06-04 05:22:38 -1200

    i added a file

---
+++ project1.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+/*
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    alert(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) alert(`you rolled a 6`)
+};*/
+
+
+
+
